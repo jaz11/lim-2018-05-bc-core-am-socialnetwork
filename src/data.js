a577bca464330b5cdeb9541a7dbf3fa792001079
Jahzell Pelaez <jahzell111@gmail.com>
2018-07-20 09:07:48 -0500

    everything clean

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-
 // FB.getLoginStatus(function(response) {
 //     statusChangeCallback(response);
 // });
-
--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const=[love:Dkom&&Jass,luck:1195]
-=======
 var hereCallJS = document.getElementById('HereCall');
 var refData = firebase.dataBase().ref().child('text');
 refData.on('value', snap => hereCallJS.innerText = snap.val());
-
-
-// {
-//     status: 'connected';
-//     authResponse: {
-//         accessToken: '...';
-//         expiresIn:'...';
-//         signedRequest:'...';
-//         userID:'...'
-//     }
-// };
->>>>>>> 6f74e10df1b88d39a4294edfc155ac84c70ff7d7
