e0be3b4fa919a3118570c72c65e62789bd73c1a1
KaluGN <kaluguzmann@gmail.com>
2018-07-21 18:30:49 -0500

    insertando bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-var hereCallJS = document.getElementById('HereCall');
-var refData = firebase.dataBase().ref().child('text');
-refData.on('value', snap => hereCallJS.innerText = snap.val());
+// let hereCallJS = document.getElementById('HereCall');
+// let refData = firebase.dataBase().ref().child('text');
+// refData.on('value', snap => hereCallJS.innerText = snap.val());
+
+let clickButtom = document.getElementById('buttonLogIn');
+
+function registrar () {
+    console.log('funciona el click');
+}
