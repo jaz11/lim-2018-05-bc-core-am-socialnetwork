1ea39fb866baebd0a36c2547cc18e82384882160
Jahzell Pelaez <jahzell111@gmail.com>
2018-07-23 09:41:58 -0500

    firebase rules added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-var hereCallJS = document.getElementById('HereCall');
-var refData = firebase.dataBase().ref().child('text');
-refData.on('value', snap => hereCallJS.innerText = snap.val());
+// var hereCallJS = document.getElementById('HereCall');
+// var refData = firebase.dataBase().ref().child('text');
+// refData.on('value', snap => hereCallJS.innerText = snap.val());
