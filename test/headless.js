72a4852f73eae250df430710fcbe64e777c77956
Kalu Guzman <33642907+KaluGN@users.noreply.github.com>
2018-08-11 14:19:47 -0500

    Revert "uniendo cambio para testear"

--- test/headless.js
+++ test/headless.js
@@ -1,6 +1,4 @@
 global.window = global;
 global.assert = require('chai').assert;
 // require('../src/data');
-//require importa archivos, devuelve un valor dependiendo de lo que expone el módulo
-require('../src/js/validations.js')
-require('./validation.spec.js');
+require('./data.spec.js');
